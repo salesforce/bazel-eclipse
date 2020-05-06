9bb82881e733efe23197966a6a352933a594b2f9
Simon Toens <stoens@salesforce.com>
2020-05-07 00:35:17 +0900

    Tweak arg splitting logic (#118)

--- plugin-libs/plugin-model/src/main/java/com/salesforce/bazel/eclipse/model/ArgumentSplitter.java
+++ plugin-libs/plugin-model/src/main/java/com/salesforce/bazel/eclipse/model/ArgumentSplitter.java
@@ -8,7 +8,7 @@ public class ArgumentSplitter {
     public List<String> split(String argumentsString) {
         // this should probably be more sophisticated, at least handle quotes...
         List<String> args = new ArrayList<>();
-        for (String arg : argumentsString.split(" ")) {
+        for (String arg : argumentsString.split("\\s+")) {
             arg = arg.trim();
             if (arg.length() > 0) {
                 args.add(arg);
--- plugin-libs/plugin-model/src/test/java/com/salesforce/bazel/eclipse/model/ArgumentSplitterTest.java
+++ plugin-libs/plugin-model/src/test/java/com/salesforce/bazel/eclipse/model/ArgumentSplitterTest.java
@@ -7,9 +7,21 @@ import java.util.List;
 import org.junit.Test;
 
 public class ArgumentSplitterTest {
+    
+    @Test
+    public void testSplit_newline() {
+        ArgumentSplitter as = new ArgumentSplitter();
+
+        List<String> args = as.split("a1\na2=v2\na3\n");
+
+        assertEquals(3, args.size());
+        assertEquals("a1", args.get(0));
+        assertEquals("a2=v2", args.get(1));
+        assertEquals("a3", args.get(2));        
+    }
 
     @Test
-    public void testSplit() {
+    public void testSplit_spaces() {
         ArgumentSplitter as = new ArgumentSplitter();
 
         List<String> args = as.split("  a1 a2=v2   a3  ");
