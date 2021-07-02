3e48153080bb2b661f76a2e75262bb759bab9ae6
Peter Laird <plaird@salesforce.com>
2021-07-02 13:18:45 -0600

    another round RC job fixes

--- .github/create-update-site-rc.sh
+++ .github/create-update-site-rc.sh
@@ -1,6 +1,6 @@
 # Script to create the update site for test release candidates
 
-mkdir update-site-release-candidate
-cp releng/p2repository/target/*.zip update-site-release-candidate
-cp -R releng/p2repository/target/repository update-site-release-candidate/update-site
-cp .github/update-site-index.html update-site-release-candidate/index.html
+mkdir update-site
+cp releng/p2repository/target/*.zip update-site
+cp -R releng/p2repository/target/repository update-site/update-site-release-candidate
+cp .github/update-site-index.html update-site/index.html
