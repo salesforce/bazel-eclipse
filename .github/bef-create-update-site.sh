ab47c8b2018ef680fe7f0d6339d01f78bd15b823
Peter Laird <plaird@salesforce.com>
2021-08-21 15:40:59 -0600

    initial CI solution for lang server releases (#297)

--- .github/bef-create-update-site.sh
+++ .github/bef-create-update-site.sh
@@ -1,6 +1,6 @@
-# Script to create the update site for official releases 
+# Script to create the update site for official releases
 
 mkdir update-site
 cp releng/p2repository/target/*.zip update-site
 cp -R releng/p2repository/target/repository update-site/update-site
-cp .github/update-site-index.html update-site/index.html
+cp .github/bef-update-site-index.html update-site/index.html
