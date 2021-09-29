8873ae418be82ccc6bdff958017ba7d60966f1ba
Peter Laird <plaird@salesforce.com>
2021-09-29 10:33:39 -0600

    release workflow testing

--- .github/bef-create-update-site-rc.sh
+++ .github/bef-create-update-site-rc.sh
@@ -1,6 +1,6 @@
-# Script to create the update site for test release candidates
+# Script to create the update site for BEF test release candidates
 
-mkdir update-site
-cp releng/p2repository/target/*.zip update-site
-cp -R releng/p2repository/target/repository update-site/update-site-release-candidate
-cp .github/bef-update-site-index.html update-site/index.html
+mkdir bef-rc-staging
+cp releng/p2repository/target/*.zip bef-rc-staging
+cp -R releng/p2repository/target/repository bef-rc-staging/update-site-release-candidate
+cp .github/bef-update-site-index.html bef-rc-staging/index.html
