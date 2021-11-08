882f77093064181adedcc52116e82b61a8654cb7
alexei shmidman <alexeishmidman@gmail.com>
2021-11-08 18:40:35 +0300

    remove obsolete preferences for main/src and test/src paths

--- bundles/com.salesforce.b2eclipse.jdt.ls/src/main/java/com/salesforce/b2eclipse/managers/B2EPreferncesManager.java
+++ bundles/com.salesforce.b2eclipse.jdt.ls/src/main/java/com/salesforce/b2eclipse/managers/B2EPreferncesManager.java
@@ -45,31 +45,9 @@ public final class B2EPreferncesManager {
      */
     private static final String IMPORT_BAZEL_ENABLED = "java.import.bazel.enabled";
 
-    /**
-     * Preference key to change java classes src path for bazel importer.
-     */
-    private static final String BAZEL_SRC_PATH = "java.import.bazel.src.path";
-
-    /**
-     * Preference key to change java classes test path for bazel importer.
-     */
-    private static final String BAZEL_TEST_PATH = "java.import.bazel.test.path";
-
-    /**
-     * Default java class src path for bazel importer.
-     */
-    private static final String BAZEL_DEFAULT_SRC_PATH = "/src/main/java";
-
-    /**
-     * Default java class test path for bazel importer.
-     */
-    private static final String BAZEL_DEFAULT_TEST_PATH = "/src/test/java";
-
     private static volatile B2EPreferncesManager instance;
 
     private boolean importBazelEnabled;
-    private String importBazelSrcPath;
-    private String importBazelTestPath;
 
     private B2EPreferncesManager() {
 
@@ -77,8 +55,6 @@ public final class B2EPreferncesManager {
 
     public void setConfiguration(Map<String, Object> configuration) {
         importBazelEnabled = MapFlattener.getBoolean(configuration, IMPORT_BAZEL_ENABLED, false);
-        importBazelSrcPath = MapFlattener.getString(configuration, BAZEL_SRC_PATH, BAZEL_DEFAULT_SRC_PATH);
-        importBazelTestPath = MapFlattener.getString(configuration, BAZEL_TEST_PATH, BAZEL_DEFAULT_TEST_PATH);
     }
 
     public static B2EPreferncesManager getInstance() {
@@ -100,14 +76,6 @@ public final class B2EPreferncesManager {
 
     public boolean isImportBazelEnabled() {
         return importBazelEnabled;
-    }
-
-    public String getImportBazelSrcPath() {
-        return importBazelSrcPath;
-    }
-
-    public String getImportBazelTestPath() {
-        return importBazelTestPath;
     }
 
 }
