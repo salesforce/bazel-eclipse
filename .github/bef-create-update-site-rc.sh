f58866bd4f1fa5d083bcbdeb0e9369bb6f76cf3d
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2023-05-25 12:15:41 +0200

    Cache Bazel repository cache

--- .github/bef-create-update-site-rc.sh
+++
@@ -1,6 +0,0 @@
-# Script to create the update site for BEF test release candidates
-
-mkdir bef-rc-staging
-cp releng/p2repository/target/*.zip bef-rc-staging
-cp -R releng/p2repository/target/repository bef-rc-staging/update-site-release-candidate
-cp .github/bef-update-site-index.html bef-rc-staging/index.html
