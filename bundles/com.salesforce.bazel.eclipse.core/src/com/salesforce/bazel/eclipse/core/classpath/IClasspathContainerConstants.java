209ebd20ff4df2945405cee74a398222965c219b
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2023-06-08 09:47:27 +0200

    Remove obsolete code

--- bundles/com.salesforce.bazel.eclipse.core/src/com/salesforce/bazel/eclipse/core/classpath/IClasspathContainerConstants.java
+++
@@ -1,6 +0,0 @@
-package com.salesforce.bazel.eclipse.core.classpath;
-
-public interface IClasspathContainerConstants {
-    String JAVA_DEBUG_DELEGATE_CMD_HANDLER = "JavaDebugDelegateCommandHandler";
-    String JUNIT_LAUNCH_CONFIGURATION_DELEGATE = "JUnitLaunchConfigurationDelegate";
-}
