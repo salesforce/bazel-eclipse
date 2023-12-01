a854bcf17ddf649c722704a432beb625608084b0
Gunnar Wagenknecht <gunnar@wagenknecht.org>
2023-12-01 15:17:49 -0800

    Fix problem with preferences not sync'd properly

--- bundles/com.salesforce.bazel.eclipse.core/src/com/salesforce/bazel/eclipse/core/model/discovery/EclipsePreferencesHelper.java
+++ bundles/com.salesforce.bazel.eclipse.core/src/com/salesforce/bazel/eclipse/core/model/discovery/EclipsePreferencesHelper.java
@@ -1,8 +1,10 @@
 package com.salesforce.bazel.eclipse.core.model.discovery;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.Properties;
 
 import org.eclipse.core.internal.preferences.EclipsePreferences;
+import org.osgi.service.prefs.BackingStoreException;
 import org.osgi.service.prefs.Preferences;
 
 /**
@@ -11,7 +13,21 @@ import org.osgi.service.prefs.Preferences;
 @SuppressWarnings("restriction")
 class EclipsePreferencesHelper extends EclipsePreferences {
 
-    public static void convertToPreferences(Properties table, Preferences targetPreferences) {
+    public static void convertToPreferences(Properties table, Preferences targetPreferences)
+            throws BackingStoreException {
         EclipsePreferences.convertFromProperties((EclipsePreferences) targetPreferences, table, true);
+        makeDirty((EclipsePreferences) targetPreferences);
+    }
+
+    private static void makeDirty(EclipsePreferences targetPreferences) throws BackingStoreException {
+        try {
+            var makeDirty = EclipsePreferences.class.getDeclaredMethod("makeDirty");
+            makeDirty.setAccessible(true);
+            makeDirty.invoke(targetPreferences);
+        } catch (NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException
+                | InvocationTargetException e) {
+            throw new BackingStoreException("Unable to mark modified preferences for needing save!", e);
+        }
+
     }
 }
